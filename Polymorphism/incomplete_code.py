3eddb1931edb6aaf80b5ff51cf99fa01405416f2
ufomysis <ufomysis@gmail.com>
2016-02-19 14:33:27 +0700

    Update incomplete_code.py

--- Polymorphism/incomplete_code.py
+++ Polymorphism/incomplete_code.py
@@ -20,6 +20,16 @@ class Walk( Transportation ):
 
    def find_cost( self ):
       return 0
+      
+      
+      
+class Taxi( Transportation ):
+
+   def __init__( self, start, end, distance ):
+      Transportation.__init__( self, start, end, distance)
+
+   def find_cost( self ):
+      return (40 * distance)
 
 
    
